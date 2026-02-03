67f79f0c1a37f3dd063dbaf9de2e10b44f7f40aa
sophek44 <122432106+sophek44@users.noreply.github.com>
2026-02-02 22:30:19 -0800

    add text interaction

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -1,30 +1,57 @@
 using UnityEngine;
+using TMPro;
 
 public class PlayerInteract : MonoBehaviour
 {
     public float range = 3f;
+    public GameObject interactionPrompt;
+    public TextMeshProUGUI promptText;
+
+    void Start()
+    {
+        // Hide the text when the game starts
+        interactionPrompt.SetActive(false);
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            AttemptInteract();
-        }
+
+        CheckForInteractable();
     }
 
-    void AttemptInteract()
+    void CheckForInteractable()
     {
         Ray ray = new Ray(transform.position, transform.forward);
 
+        // 1. SHOOT THE LASER
         if (Physics.Raycast(ray, out RaycastHit hit, range))
         {
             TimeTravel timeScript = hit.collider.GetComponent<TimeTravel>();
 
             if (timeScript != null)
             {
-                timeScript.Interact();
+
+                interactionPrompt.SetActive(true);
+
+                // Update what the text says
+                if (hit.collider.CompareTag("TutorialObelisk"))
+                {
+                    promptText.text = "Press E to interact";
+                }
+                else
+                {
+                    promptText.text = "E";
+                }
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    timeScript.Interact();
+                }
+
+                return; 
             }
         }
-    }
 
+        interactionPrompt.SetActive(false);
+    }
 }
