ab90304a6cfa8026fe5c6e36668568135ed3ceac
James Malvar <jbmalvar@cs.washington.edu>
2026-02-03 01:40:31 -0800

    DoorAnimation added but the collider ain't on

---
+++ Assets/Scripts/DoorInteraction.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class DoorInteraction : MonoBehaviour
+{
+    public float openAngle = 90f;
+    public float openSpeed = 2f;
+    private bool isOpen = false;
+
+    private Quaternion closedRotation;
+    private Quaternion openRotation;
+    private Coroutine currentCoroutine;
+
+    void Start()
+    {
+        closedRotation = transform.rotation;
+        openRotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0, openAngle, 0));
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentCoroutine != null)
+            {
+                StopCoroutine(currentCoroutine);
+            }
+            currentCoroutine = StartCoroutine(ToggleDoor());
+        }
+    }
+
+    private IEnumerator ToggleDoor()
+    {
+        Quaternion targetRotation = isOpen ? closedRotation : openRotation;
+        isOpen = !isOpen;
+
+        while (Quaternion.Angle(transform.rotation, targetRotation) > 0.1f)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * openSpeed);
+            yield return null;
+        }
+
+        transform.rotation = targetRotation;
+    }
+}
