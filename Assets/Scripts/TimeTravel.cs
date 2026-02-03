8fa7cf233fc6ff10bdd1eafcd4d2059e87b1ed51
sophek44 <122432106+sophek44@users.noreply.github.com>
2026-02-02 18:30:45 -0800

    Add obelisk interaction

---
+++ Assets/Scripts/TimeTravel.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TimeTravel : MonoBehaviour
+{
+    [SerializeField] GameObject present, past;
+    //[SerializeField] bool presentIsVisible = true;
+
+
+    void Start()
+    {
+        present.SetActive(true);
+        past.SetActive(false);
+    }
+
+    public void Interact()
+    {
+        bool isPresentActive = present.activeSelf;
+
+        present.SetActive(!isPresentActive);
+        past.SetActive(isPresentActive);
+    }
+
+}
