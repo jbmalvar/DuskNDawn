8c9a02a9a02926d48e0bc30d3e4efa48b3bc6df4
James Malvar <jbmalvar@cs.washington.edu>
2026-02-08 13:08:36 -0800

    Bug

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -6,37 +6,45 @@ public class PlayerInteract : MonoBehaviour
     public float range = 3f;
     public GameObject interactionPrompt;
     public TextMeshProUGUI promptText;
+    
+    // Set this to "Player" or "Ignore Raycast" in the Inspector
+    [SerializeField] private LayerMask ignoreLayer; 
+
+    private Camera cam;
 
     void Start()
     {
-        // Hide the text when the game starts
+        cam = Camera.main; 
         interactionPrompt.SetActive(false);
+        
+        // Safety check to make sure you have a camera tagged "MainCamera"
+        if (cam == null) Debug.LogError("No Main Camera found in scene!");
     }
 
     void Update()
     {
-
         CheckForInteractable();
     }
 
     void CheckForInteractable()
     {
-        Ray ray = new Ray(transform.position, transform.forward);
+        // This shoots the ray from the EXACT center of your screen
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        
+        // Debug: See the ray in the Scene view while playing
+        Debug.DrawRay(ray.origin, ray.direction * range, Color.green);
 
-        if (Physics.Raycast(ray, out RaycastHit hit, range))
+        // ~ignoreLayer ensures the ray doesn't hit the player/torch in your hand
+        if (Physics.Raycast(ray, out RaycastHit hit, range, ~ignoreLayer))
         {
-            // Check for TimeTravel (Existing logic)
-            TimeTravel timeScript = hit.collider.GetComponent<TimeTravel>();
-            // Check for Torch (New logic)
-            TorchPickup torchScript = hit.collider.GetComponent<TorchPickup>();
-
-            if (timeScript != null)
+            if (hit.collider.TryGetComponent(out TimeTravel timeScript))
             {
                 ShowPrompt("Press E to interact");
                 if (Input.GetKeyDown(KeyCode.E)) timeScript.Interact();
                 return;
             }
-            else if (torchScript != null)
+            
+            if (hit.collider.TryGetComponent(out TorchPickup torchScript))
             {
                 ShowPrompt("Press E to pick up Torch");
                 if (Input.GetKeyDown(KeyCode.E)) torchScript.PickUp();
@@ -47,7 +55,6 @@ public class PlayerInteract : MonoBehaviour
         interactionPrompt.SetActive(false);
     }
 
-    // Small helper to keep things clean
     void ShowPrompt(string message)
     {
         interactionPrompt.SetActive(true);
